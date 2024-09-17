192378456008cebcdb9d93a599d45772ab478033
Metiko Temitope <temitopemetiko@gmail.com>
2024-09-17 15:32:09 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "private" {
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   availability_zone      = "${var.region}a"
   vpc_security_group_ids = [aws_security_group.docker_on_ec2.id, ]
-  key_name               = var.ssh_key
+  key_name               = ssh=key
 
   # User data script to configure docker 
   user_data = <<-EOF
